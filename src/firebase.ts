0c5e826f50b4ccd4cb8035e7145cdec4b0923aac
Carlos Laran <carloooseeen@gmail.com>
2026-04-11 02:12:31 +0800

    secrets hidden fuckkk

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZoXSshImeHvs1njS92kARvJluAj96WBY",
-  authDomain: "lab-prep-df2fe.firebaseapp.com",
-  projectId: "lab-prep-df2fe",
-  storageBucket: "lab-prep-df2fe.firebasestorage.app",
-  messagingSenderId: "507427448426",
-  appId: "1:507427448426:web:5e1a22a8492218ea6d4c13",
-  measurementId: "G-2LH418BC2P"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
